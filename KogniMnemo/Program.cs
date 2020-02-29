5a9fbc606f13675420fb30db8529e2f8fe833c84
Sandro <Siegfried1997@gmail.com>
2020-02-29 17:51:30 +0300

    Add project files.

---
+++ KogniMnemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KogniMnemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
