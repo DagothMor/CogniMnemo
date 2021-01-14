0c79a5ae968735da6021742b6d7f800e38278959
DagothMor <siegfried1997@gmail.com>
2021-01-14 15:15:48 +0300

    Blank

--- KogniMnemo/Program.cs
+++ KogniMnemo/Program.cs
@@ -3,12 +3,33 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace KogniMnemo
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-        }
-    }
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine("Welcome to the CorgiMneemo!" +
+					"The best application for training your skills!");
+				Console.WriteLine("Main menu:" +
+					"1-Start new game!" +
+					"2-Add a new note." +
+					"3-Exit");
+				var input = Console.ReadLine();
+				if (int.Parse(input) == 3)//exit from game.
+				{
+					Environment.Exit(0);
+				}
+				else if (int.Parse(input) == 1)//Start new game.
+				{
+				}
+				else if (int.Parse(input) == 2)//Add a note.
+				{
+
+				}
+				Console.Clear();
+			}
+		}
+	}
 }
